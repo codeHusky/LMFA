1bdbdc679593f269f6d31aa7f9d72a0f601d7a9d
MikaalSky <lokio.casebstv@gmail.com>
2016-04-25 10:09:02 -0500

    Somewhat working on https

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ chrome.webRequest.onBeforeRequest.addListener(
     	console.log(details.url);
         if( details.url.toString().indexOf("main_out.js") > -1 && details.url.toString().indexOf("pass") == -1){
         	console.log("Blocked: " + details.url)
-            return {redirectUrl: "http://agar.io/js/jquery.js" };
-        }else if(details.url.toString().indexOf("magicturtle.js") > -1){
-        	return {redirectUrl: "http://agar.io/main_out.js?pass" };
+            //return {redirectUrl: "https://agar.io/js/jquery.js" };
+        }else if(details.url.toString() == "http://agar.io/"){
+            return {redirectUrl:"javascript:"};
+            console.log("close")
         }
     },
-    {urls: ["*://agar.io/*.js"]},
+    {urls: ["*://agar.io/*.*","*://agar.io/"]},
     ["blocking"]);
